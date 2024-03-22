7e3e70f9b84af348b4db28c432483ae20be8ea0c
Michael Alu <alumichael9@gmail.com>
2024-03-22 04:01:53 +0100

    Update firebase_config.dart

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -13,6 +13,6 @@ Future initFirebase() async {
             appId: "1:15839972586:web:b863d970bc461731139b93",
             measurementId: "G-XR46HPE6Q4"));
   } else {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   }
 }
